b083560feffa11f146fd0761d82e0480057eab76
Mark Kil <markkilcanarias@gmail.com>
2023-12-24 20:02:02 +0100

    basic form structure ready, no submit button

---
+++ src/components/login-view/LoginForm.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <form action="submit">
+        <h3>USERNAME</h3>
+        <input id="username-input" type="text">
+
+        <h3>PASSWORD</h3>
+        <input id="password-input" type="text">
+    </form>
+</template>
+
+<style scoped lang="scss">
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 40%;
+    width: 90%;
+    background-color: $base-grey;
+    padding-top: 10%;
+}
+
+h3 {
+    font-family: "Cyberdyne Halftone";
+    font-style: normal;
+    font-size: 2vmax;
+    color: white;
+    margin: 0% 0 10% 0;
+}
+
+input {
+    width: 90%;
+    background-color: black;
+    font-family: 'VT323', monospace;
+    color: $text-green;
+    margin-bottom: 10%;
+    height: 10%;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import LoginForm from './../components/login-view/LoginForm.vue'
+</script>
+
+<template>
+  <main>
+    <h1 id="page-title">TECHNICAL SUPPORT</h1>
+    <img id="terminator-gif" src="/images/gif/wayne-dalton-wd-terminator-32c-2019-anim.gif" alt="t-800 image">
+    <h2 id="view-title">LOGIN</h2>
+
+    <LoginForm />
+
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  width: 100vw;
+  height: 90vh;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#page-title {
+  display: block;
+  font-family: "Cyberdyne";
+  font-size: 2.2vmax;
+  color: white;
+  margin: 5% 0 5% 0;
+}
+
+#view-title {
+  display: block;
+  font-family: "Cyberdyne";
+  font-size: 2.2vmax;
+  color: white;
+  margin: 5% 0 5% 0;
+}
+
+#terminator-gif {
+  height: 20%;
+}
+</style>
